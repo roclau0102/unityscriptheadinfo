d0ae62f68a672fd91ba7d0e4947f42d47a80b636
roc <137887850@qq.com>
2017-12-20 10:37:32 +0800

    Add files via upload

---
+++ Editor/AddFileHeadComment.cs
@@ -0,0 +1,43 @@
+﻿/****************************************************************
+ *Copyright(C) 2017 by #COMPANY# All rights reserved. 
+ *FileName:     #SCRIPTFULLNAME# 
+ *Author:       #AUTHOR# 
+ *Version:      #VERSION# 
+ *UnityVersion：#UNITYVERSION# 
+ *Date:         #DATE# 
+ *Description:    
+ *History: 
+*****************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AddFileHeadComment : AssetModificationProcessor
+{
+
+    static void OnWillCreateAsset(string newFileMeta)
+    {
+        string newFilePath = newFileMeta.Replace(".meta", "");
+        string fileExt = Path.GetExtension(newFilePath);
+        if (fileExt != ".cs")
+        {
+            return;
+        }
+        //注意，Application.datapath会根据使用平台不同而不同  
+        string realPath = Application.dataPath.Replace("Assets", "") + newFilePath;
+        string scriptContent = File.ReadAllText(realPath);
+
+        //这里实现自定义的一些规则  
+        scriptContent = scriptContent.Replace("#SCRIPTFULLNAME#", Path.GetFileName(newFilePath));
+        scriptContent = scriptContent.Replace("#COMPANY#", PlayerSettings.companyName);
+        scriptContent = scriptContent.Replace("#AUTHOR#", "roc");
+        scriptContent = scriptContent.Replace("#VERSION#", "1.0");
+        scriptContent = scriptContent.Replace("#UNITYVERSION#", Application.unityVersion);
+        scriptContent = scriptContent.Replace("#DATE#", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
+
+        File.WriteAllText(realPath, scriptContent);
+    }
+}
